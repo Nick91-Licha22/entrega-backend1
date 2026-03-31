efbd3149dcf860f3b42d371ba255ffee5fe48cfb
Nicolas Andino <nicolas.n.andino@gmail.com>
2026-03-30 23:36:40 -0300

    Nueva_Entrega_Final

--- src/dao/mongo/products.mongo.js
+++ src/dao/mongo/products.mongo.js
@@ -1,25 +1,9 @@
-import productModel from "../../models/product.model.js";
+import productModel from "../../models/product.model.js"; 
 
 export default class Products {
-    get = async() => {
-        return await productModel.find();
-    }
-    getById = async(id) => {
-        return await productModel.findById(id);
-    }
-
-    // Crear un producto nuevo
-    create = async(data) => {
-        return await productModel.create(data);
-    }
-
-    // Actualizar (Stock, precio, etc.)
-    update = async(id, data) => {
-        return await productModel.findByIdAndUpdate(id, data, { new: true });
-    }
-
-    // Eliminar
-    delete = async(id) => {
-        return await productModel.findByIdAndDelete(id);
-    }
+    get = async() => await productModel.find();
+    getById = async(id) => await productModel.findById(id);
+    create = async(data) => await productModel.create(data);
+    update = async(id, data) => await productModel.findByIdAndUpdate(id, data, { new: true });
+    delete = async(id) => await productModel.findByIdAndDelete(id);
 }
