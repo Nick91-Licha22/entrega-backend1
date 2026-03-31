53cdf0b83b1a702ee545f6867e0cb88d337b8722
Nicolas Andino <nicolas.n.andino@gmail.com>
2026-03-30 23:33:09 -0300

    Nueva_Entrega_Final

--- src/dao/mongo/carts.mongo.js
+++ src/dao/mongo/carts.mongo.js
@@ -1,19 +1,8 @@
 import cartModel from "../../models/cart.model.js";
 
 export default class Carts {
-    get = async() => {
-        return await cartModel.find().populate('products.product');
-    }
-
-    getById = async(id) => {
-        return await cartModel.findById(id).populate('products.product');
-    }
-
-    create = async() => {
-        return await cartModel.create({ products: [] });
-    }
-
-    update = async(id, data) => {
-        return await cartModel.findByIdAndUpdate(id, data, { new: true });
-    }
+    get = async() => await cartModel.find().populate('products.product');
+    getById = async(id) => await cartModel.findById(id).populate('products.product');
+    create = async() => await cartModel.create({ products: [] });
+    update = async(id, data) => await cartModel.findByIdAndUpdate(id, data, { new: true });
 }
